b9a45ed1250c42095aa6a97e5094803a038aeb9e
id2k1149 <id2k1149@gmail.com>
2023-04-27 09:30:22 -0400

    add initial view

--- SliderGame/ContentView.swift
+++ SliderGame/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 32) {
+            HStack {
+                Text("Подвинь слайдер, как можно ближе к: ")
+                Text("82")
+            }
+            HStack {
+                Text("0")
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                Text("100")
+            }
+           
+            Button("Проверь меня") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Button("Начать заново") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
         .padding()
     }
