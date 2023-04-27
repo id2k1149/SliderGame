3dd58aee1738e24d8722d8965d4e658ef20c0a5b
id2k1149 <id2k1149@gmail.com>
2023-04-27 09:22:32 -0400

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Max Franz Immelmann on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
